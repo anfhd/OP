98264c3ae64e2c1eecb822d414fb867fd7a0261d
Yukimora <95708691+anfhd@users.noreply.github.com>
2022-03-19 06:13:12 +0200

    :(

---
+++ lab_1/task_2.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x, y, z, max, min;
+	scanf("%f %f %f", &x, &y, &z);
+	max = x;
+	min = y;
+	if (y > max) max = y;
+	if (z > max) max = z;
+	if (z < min) min = z;
+	printf("%f", (max - x * min) / (x * y * z + x * x + y * y * y));
+	//не використовуйте pow() для піднімання до квадрату\кубу
+}
