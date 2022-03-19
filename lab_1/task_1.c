b5f629586971896bb7a0f1b027bd635f396e0a68
Yukimora <95708691+anfhd@users.noreply.github.com>
2022-03-19 06:06:30 +0200

    :/

---
+++ lab_1/task_1.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x, y;
+	scanf("%f", &x);
+	scanf("%f", &y);
+	printf("%f", pow(y, x) + sqrt((fabs(x) + fabs(y))));
+}
